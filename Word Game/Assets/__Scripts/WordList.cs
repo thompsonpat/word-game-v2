498b68dc11a8aad89bca78980df4b8535aa736b5
thompsonpat <thompsonpat21@gmail.com>
2018-11-20 00:07:31 -0500

    Parsing the Word List

--- Word Game/Assets/__Scripts/WordList.cs
+++ Word Game/Assets/__Scripts/WordList.cs
@@ -2,15 +2,118 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WordList : MonoBehaviour {
+public class WordList : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static WordList S;
+
+    [Header("Set in Inspector")]
+    public TextAsset wordListText;
+    public int numToParseBeforeYield = 10000;
+    public int wordLengthMin = 3;
+    public int wordLengthMax = 7;
+
+    [Header("Set Dynamically")]
+    public int currLine = 0;
+    public int totalLines;
+    public int longWordCount;
+    public int wordCount;
+
+    // Private Fields
+    private string[] lines;
+    private List<string> longWords;
+    private List<string> words;
+
+    void Awake()
+    {
+        S = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        lines = wordListText.text.Split('\n');
+        totalLines = lines.Length;
+
+        StartCoroutine(ParseLines());
+    }
+
+    public IEnumerator ParseLines()
+    {
+        string word;
+
+        longWords = new List<string>();
+        words = new List<string>();
+
+        for (currLine = 0; currLine < totalLines; currLine++)
+        {
+            word = lines[currLine];
+			
+            if (word.Length == wordLengthMax)
+            {
+                longWords.Add(word);
+            }
+
+            if (word.Length >= wordLengthMin && word.Length <= wordLengthMax)
+            {
+                words.Add(word);
+            }
+
+            if (currLine % numToParseBeforeYield == 0)
+            {
+                longWordCount = longWords.Count;
+                wordCount = words.Count;
+
+                yield return null;
+            }
+        }
+
+        longWordCount = longWords.Count;
+        wordCount = words.Count;
+    }
+
+    static public List<string> GET_WORDS()
+    {
+        return (S.words);
+    }
+
+    static public string GET_WORD(int ndx)
+    {
+        return (S.words[ndx]);
+    }
+
+    static public List<string> GET_LONG_WORDS()
+    {
+        return S.longWords;
+    }
+
+    static public string GET_LONG_WORD(int ndx)
+    {
+        return (S.longWords[ndx]);
+    }
+
+    static public int WORD_COUNT
+    {
+        get { return S.wordCount; }
+    }
+
+    static public int LONG_WORD_COUNT
+    {
+        get { return S.longWordCount; }
+    }
+
+    static public int NUM_TO_PARSE_BEFORE_YIELD
+    {
+        get { return S.numToParseBeforeYield; }
+    }
+
+    static public int WORD_LENGTH_MIN
+    {
+        get { return S.wordLengthMin; }
+    }
+
+    static public int WORD_LENGTH_MAX
+    {
+        get { return S.wordLengthMax; }
+    }
 }
