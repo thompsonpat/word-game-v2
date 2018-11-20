5fd114a723d84a1ede4dbbc61277f564d1b84e6e
thompsonpat <thompsonpat21@gmail.com>
2018-11-20 00:26:56 -0500

    Building a Level with the WordLevel Class

--- Word Game/Assets/__Scripts/WordGame.cs
+++ Word Game/Assets/__Scripts/WordGame.cs
@@ -15,25 +15,91 @@ public enum GameMode
 public class WordGame : MonoBehaviour
 {
 
-	static public WordGame S;
+    static public WordGame S;
 
-	[Header("Set Dynamically")]
+    [Header("Set Dynamically")]
     public GameMode mode = GameMode.preGame;
+    public WordLevel currLevel;
 
-	void Awake()
-	{
-		S = this;
-	}
+    void Awake()
+    {
+        S = this;
+    }
 
     void Start()
     {
-		mode = GameMode.loading;
+        mode = GameMode.loading;
 
-		WordList.INIT();
+        WordList.INIT();
     }
 
-	public void WordListParseComplete()
+    public void WordListParseComplete()
     {
         mode = GameMode.makeLevel;
+
+        currLevel = MakeWordLevel();
+    }
+
+    public WordLevel MakeWordLevel(int levelNum = -1)
+    {
+        WordLevel level = new WordLevel();
+        if (levelNum == -1)
+        {
+			// Pick a random level
+            level.longWordIndex = Random.Range(0, WordList.LONG_WORD_COUNT);
+        }
+        else
+        {
+
+        }
+
+        level.levelNum = levelNum;
+        level.word = WordList.GET_LONG_WORD(level.longWordIndex);
+        level.charDict = WordLevel.MakeCharDict(level.word);
+
+        StartCoroutine(FindSubWordsCoroutine(level));
+
+        return level;
+    }
+
+	public IEnumerator FindSubWordsCoroutine(WordLevel level)
+    {
+        level.subWords = new List<string>();
+        string str;
+
+        List<string> words = WordList.GET_WORDS();
+
+        for (int i = 0; i < WordList.WORD_COUNT; i++)
+        {
+            str = words[i];
+
+            if (WordLevel.CheckWordInLevel(str, level))
+            {
+                level.subWords.Add(str);
+            }
+
+            if (i % WordList.NUM_TO_PARSE_BEFORE_YIELD == 0)
+            {
+                yield return null;
+            }
+        }
+
+        level.subWords.Sort();
+        level.subWords = SortWordsByLength(level.subWords).ToList();
+
+        // The coroutine is complete, so call SubWordSearchComplete()
+        SubWordSearchComplete();
+    }
+
+	// Use LINQ to sortthe array received and return a copy
+    public static IEnumerable<string> SortWordsByLength(IEnumerable<string> ws)
+    {
+        ws = ws.OrderBy(s => s.Length);
+        return ws;
+    }
+
+	public void SubWordSearchComplete()
+    {
+        mode = GameMode.levelPrep;
     }
 }
---
+++ Word Game/Assets/__Scripts/WordLevel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WordLevel
+{
+    public int levelNum;
+    public int longWordIndex;
+    public string word;
+
+    // A dict. of all the Letters in word
+    public Dictionary<char, int> charDict;
+
+    // All the words that can be spelled with the Letters in charDict
+    public List<string> subWords;
+
+    static public Dictionary<char, int> MakeCharDict(string w)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        char c;
+
+        for (int i = 0; i < w.Length; i++)
+        {
+            c = w[i];
+            if (dict.ContainsKey(c))
+            {
+                dict[c]++;
+            }
+            else
+            {
+                dict.Add(c, 1);
+            }
+        }
+        return dict;
+    }
+
+    public static bool CheckWordInLevel(string str, WordLevel level)
+    {
+        Dictionary<char, int> counts = new Dictionary<char, int>();
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            char c = str[i];
+
+            if (level.charDict.ContainsKey(c))
+            {
+
+                if (!counts.ContainsKey(c))
+                {
+                    counts.Add(c, 1);
+                }
+                else
+                {
+                    counts[c]++;
+                }
+
+                if (counts[c] > level.charDict[c])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
