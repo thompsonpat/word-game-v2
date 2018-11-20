29a9a40b918bae37d4352fc9c3492bc6d9cd0cd2
thompsonpat <thompsonpat21@gmail.com>
2018-11-20 01:03:36 -0500

    The WordGame.Layout() Method

--- Word Game/Assets/__Scripts/WordGame.cs
+++ Word Game/Assets/__Scripts/WordGame.cs
@@ -17,13 +17,25 @@ public class WordGame : MonoBehaviour
 
     static public WordGame S;
 
+    [Header("Set in Inspector")]
+    public GameObject prefabLetter;
+    public Rect wordArea = new Rect(-24, 19, 48, 28);
+    public float letterSize = 1.5f;
+    public bool showAllWyrds = true;
+
+
     [Header("Set Dynamically")]
     public GameMode mode = GameMode.preGame;
     public WordLevel currLevel;
+    public List<Wyrd> wyrds;
+
+    private Transform letterAnchor, bigLetterAnchor;
 
     void Awake()
     {
         S = this;
+        letterAnchor = new GameObject("LetterAnchor").transform;
+        bigLetterAnchor = new GameObject("BigLetterAnchor").transform;
     }
 
     void Start()
@@ -45,7 +57,7 @@ public class WordGame : MonoBehaviour
         WordLevel level = new WordLevel();
         if (levelNum == -1)
         {
-			// Pick a random level
+            // Pick a random level
             level.longWordIndex = Random.Range(0, WordList.LONG_WORD_COUNT);
         }
         else
@@ -62,7 +74,7 @@ public class WordGame : MonoBehaviour
         return level;
     }
 
-	public IEnumerator FindSubWordsCoroutine(WordLevel level)
+    public IEnumerator FindSubWordsCoroutine(WordLevel level)
     {
         level.subWords = new List<string>();
         string str;
@@ -91,15 +103,77 @@ public class WordGame : MonoBehaviour
         SubWordSearchComplete();
     }
 
-	// Use LINQ to sortthe array received and return a copy
+    // Use LINQ to sortthe array received and return a copy
     public static IEnumerable<string> SortWordsByLength(IEnumerable<string> ws)
     {
         ws = ws.OrderBy(s => s.Length);
         return ws;
     }
 
-	public void SubWordSearchComplete()
+    public void SubWordSearchComplete()
     {
         mode = GameMode.levelPrep;
+        Layout();
+    }
+
+    void Layout()
+    {
+        // Place the Letters for each subword of currLevel on screen
+        wyrds = new List<Wyrd>();
+
+        GameObject go;
+        Letter lett;
+        string word;
+        Vector3 pos;
+        float left = 0;
+        float columnWidth = 3;
+        char c;
+        Color col;
+        Wyrd wyrd;
+
+        // Determine how many rows of Letters will fit on screen
+        int numRows = Mathf.RoundToInt(wordArea.height / letterSize);
+
+        // Make a Wyrd of each level.subWord
+        for (int i = 0; i < currLevel.subWords.Count; i++)
+        {
+            wyrd = new Wyrd();
+            word = currLevel.subWords[i];
+
+            // if the word is longer than columnWidth, expand it
+            columnWidth = Mathf.Max(columnWidth, word.Length);
+
+            // Instantiate a PrefabLetter for each letter of the word
+            for (int j = 0; j < word.Length; j++)
+            {
+                c = word[j];
+                go = Instantiate<GameObject>(prefabLetter);
+                go.transform.SetParent(letterAnchor);
+                lett = go.GetComponent<Letter>();
+                lett.c = c;
+
+                // Position the Letter
+                pos = new Vector3(wordArea.x + left + j * letterSize, wordArea.y, 0);
+
+				// The % here makes multiple columns line up
+				pos.y -= (i % numRows) * letterSize;
+
+                lett.pos = pos;
+
+                go.transform.localScale = Vector3.one * letterSize;
+
+                wyrd.Add(lett);
+            }
+
+            if (showAllWyrds) wyrd.visible = true;
+
+            wyrds.Add(wyrd);
+
+            // If we've gotten to the numRows(th) row, start a new column
+            if (i % numRows == numRows - 1)
+            {
+                left += (columnWidth + 0.5f) * letterSize;
+            }
+        }
     }
 }
